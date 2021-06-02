0a591027e43dfb37bd4b277f13bc77474c77ccdf
rizqitsani <rizqitsani@gmail.com>
2021-06-02 13:55:57 +0700

    feat: change start node and end node appearance

--- src/components/Node/Node.jsx
+++ src/components/Node/Node.jsx
@@ -10,22 +10,20 @@ const Node = ({
   onMouseUp,
   row,
 }) => {
-  const extraClassName = isFinish
-    ? 'node-finish'
-    : isStart
-    ? 'node-start'
-    : isWall
-    ? 'node-wall'
-    : '';
+  const extraClassName = isWall ? 'node-wall' : '';
 
   return (
     <div
       id={`node-${row}-${col}`}
-      className={`node ${extraClassName}`}
+      className={`node ${extraClassName} relative`}
       onMouseDown={() => onMouseDown(row, col)}
       onMouseEnter={() => onMouseEnter(row, col)}
       onMouseUp={() => onMouseUp()}
-    />
+    >
+      <span role='img' className='absolute inset-0'>
+        {isFinish ? '🧀' : isStart ? '🐁' : ''}
+      </span>
+    </div>
   );
 };
 
