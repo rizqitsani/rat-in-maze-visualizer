dc6649dcb37634fc324860cae891260679ee039a
rizqitsani <rizqitsani@gmail.com>
2021-06-02 08:24:29 +0700

    feat: finish toolbar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
 import { useState, useEffect } from 'react';
 
 import './App.css';
-import Node from './components/Node';
-
-const START_NODE_ROW = 10;
-const START_NODE_COL = 15;
-const FINISH_NODE_ROW = 10;
-const FINISH_NODE_COL = 35;
-
-const getInitialGrid = () => {
-  const grid = [];
-  for (let row = 0; row < 20; row++) {
-    const currentRow = [];
-    for (let col = 0; col < 50; col++) {
-      currentRow.push(createNode(col, row));
-    }
-    grid.push(currentRow);
-  }
-  return grid;
-};
-
-const createNode = (col, row) => {
-  return {
-    col,
-    row,
-    isStart: row === START_NODE_ROW && col === START_NODE_COL,
-    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
-    distance: Infinity,
-    isVisited: false,
-    isWall: false,
-    previousNode: null,
-  };
-};
+import Input from './components/Input/Input';
+import Node from './components/Node/Node';
 
 const getNewGridWithWallToggled = (grid, row, col) => {
   const newGrid = grid.slice();
@@ -45,13 +16,77 @@ const getNewGridWithWallToggled = (grid, row, col) => {
 };
 
 const App = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 1, y: 4 });
+  const [cheesePosition, setCheesePosition] = useState({ x: 2, y: 6 });
   const [grid, setGrid] = useState([]);
   const [isMousePressed, setIsMousePressed] = useState(false);
 
   useEffect(() => {
+    const getInitialGrid = () => {
+      const grid = [];
+      for (let row = 1; row <= 10; row++) {
+        const currentRow = [];
+        for (let col = 1; col <= 10; col++) {
+          currentRow.push(createNode(col, row));
+        }
+        grid.push(currentRow);
+      }
+      return grid;
+    };
+
+    const createNode = (col, row) => {
+      if (col === row) console.log(row, mousePosition.x);
+      return {
+        col,
+        row,
+        isStart: row === mousePosition.x && col === mousePosition.y,
+        isFinish: row === cheesePosition.x && col === cheesePosition.y,
+        distance: Infinity,
+        isVisited: false,
+        isWall: false,
+        previousNode: null,
+      };
+    };
+
     const grid = getInitialGrid();
     setGrid(grid);
-  }, []);
+  }, [mousePosition, cheesePosition]);
+
+  const handleMouseXChange = (event) => {
+    setMousePosition((prevState) => {
+      return {
+        ...prevState,
+        x: event.target.value ? parseInt(event.target.value) : 0,
+      };
+    });
+  };
+
+  const handleMouseYChange = (event) => {
+    setMousePosition((prevState) => {
+      return {
+        ...prevState,
+        y: event.target.value ? parseInt(event.target.value) : 0,
+      };
+    });
+  };
+
+  const handleCheeseXChange = (event) => {
+    setCheesePosition((prevState) => {
+      return {
+        ...prevState,
+        x: event.target.value ? parseInt(event.target.value) : 0,
+      };
+    });
+  };
+
+  const handleCheeseYChange = (event) => {
+    setCheesePosition((prevState) => {
+      return {
+        ...prevState,
+        y: event.target.value ? parseInt(event.target.value) : 0,
+      };
+    });
+  };
 
   const handleMouseDown = (row, col) => {
     const newGrid = getNewGridWithWallToggled(grid, row, col);
@@ -107,9 +142,44 @@ const App = () => {
 
   return (
     <div className='App'>
-      <button onClick={() => this.visualizeDijkstra()}>
-        Visualize Dijkstra's Algorithm
-      </button>
+      <div className='flex py-6 mx-auto'>
+        <div className='flex items-center space-x-4 mr-20'>
+          <label>&#128001;</label>
+          <Input
+            value={mousePosition.x}
+            onChange={(e) => handleMouseXChange(e)}
+          />
+          <Input
+            value={mousePosition.y}
+            onChange={(e) => handleMouseYChange(e)}
+          />
+        </div>
+        <div className='space-x-4'>
+          <button
+            className='px-4 py-2 text-sm font-medium text-white bg-lightblue-600 border border-transparent rounded-md shadow-sm hover:bg-lightblue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lightblue-500'
+            onClick={() => this.visualizeDijkstra()}
+          >
+            Visualize A* Search
+          </button>
+          <button
+            className='px-4 py-2 text-sm font-medium text-lightblue-600 border-2 border-lightblue-600 rounded-md shadow-sm hover:text-white hover:bg-lightblue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lightblue-500'
+            onClick={() => this.visualizeDijkstra()}
+          >
+            Reset
+          </button>
+        </div>
+        <div className='flex items-center space-x-4 ml-20'>
+          <label>&#129472;</label>
+          <Input
+            value={cheesePosition.x}
+            onChange={(e) => handleCheeseXChange(e)}
+          />
+          <Input
+            value={cheesePosition.y}
+            onChange={(e) => handleCheeseYChange(e)}
+          />
+        </div>
+      </div>
       <div className='grid'>
         {grid.map((row, rowIdx) => {
           return (
